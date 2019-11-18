4ab67cc22df5a2e2d87efc45c4d2162cf65b810a
phenix1229 <paul.garay@codeimmersives.com>
2019-11-18 11:14:38 -0500

    finished phone media query

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const navLinks = document.querySelectorAll('#navBox > a');
-
-function darkenButton(){
-    // event.target.style.backGroundColor = 'black';
-    event.target.style.color = 'white';
-}
-
-navLinks.forEach(item => {
-    item.addEventListener('onmouseover', darkenButton);
+document.querySelectorAll('#gallery > .photo > img').forEach(function (pic){
+    const thing = document.querySelector('.photo');
+    // console.log(thing)
+    // const picInfo = document.querySelector('.img').innerHTML;
+    // console.log(picInfo);
+    // const pic = document.querySelector('.img > img');
+    const iLink = document.createElement('a');
+    // const picThumb = document.createElement('img')
+    iLink.target = '_blank';
+    iLink.href = pic.src; 
+    // picThumb.src = picInfo
+    iLink.innerHTML = pic.outerHTML
+    // thing.removeChild(pic);
+    thing.replaceChild(iLink, pic);
+    // thing.appendChild(iLink);
 })
-
-function fadeIn(){
-    event.target.style.opacity = '50'
-}
-const awsLogo = document.querySelector('#ads #aws');
-
-awsLogo.addEventListener('load', fadeIn)
